0d3718b146987a765a2e87f38dfd69d68a640058
Reiner Huechting <dev@reinerhuechting.de>
2025-04-29 11:50:10 +0200

    Add method stubs.

--- src/carpark.cpp
+++ src/carpark.cpp
@@ -1,1 +1,15 @@
 #include "carpark.h"
+
+CarPark::CarPark(std::vector<std::vector<std::string>> const& ids) { }
+
+bool CarPark::hasId(size_t deck_no, std::string const& id) { }
+
+bool CarPark::isOccupied(std::string const& id) { }
+
+size_t CarPark::vacant() { }
+
+size_t CarPark::occupied() { }
+
+size_t CarPark::size() { }
+
+int CarPark::parkingFee() { }
